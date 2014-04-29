5b247cc18b0a36bd8781a13b259c70390d3a5a3b
Jacob Minshall <jacob@cozybit.com>
2014-04-28 17:25:46 -0700

    platformOps friendly multichan config

--- platform/mbss_multichan/wtfconfig.py
+++ platform/mbss_multichan/wtfconfig.py
@@ -1,5 +1,6 @@
-import wtf.node.mesh
 import wtf.comm
+from wtf.node import PlatformOps
+import wtf.node.mesh
 
 subnet = "192.168.34"
 meshid = "meshmesh"
@@ -11,6 +12,7 @@ zotacs = []
 for n in range(10, 13):
 # comms
     z_ssh = wtf.comm.SSH(ipaddr="10.10.10." + str(n))
+    ops = PlatformOps(z_ssh)
     z_ssh.name = "zotac-" + str(n)
     z_ssh.verbosity = 2
 
@@ -20,16 +22,18 @@ for n in range(10, 13):
     ifaces = []
 
 # iface + ip
-    ifaces.append(wtf.node.Iface(name="wlan0", driver="ath9k", ip="%s.%d" %
-                  (subnet, 10 + n)))
+    ifaces.append(wtf.node.Iface.create_iface(name="wlan0", driver="ath9k",
+                                              ip="%s.%d" % (subnet, 10 + n),
+                                              ops=ops))
 # BSS
     ifaces[-1].conf = wtf.node.mesh.MeshConf(ssid=meshid, channel=channel,
                                              htmode=htmode, iface=ifaces[-1], shared=False)
 
 # "middle" node
     if n == 11:
-        ifaces.append(
-            wtf.node.Iface(name="wlan1", driver="ath9k", ip="%s.%d" % (subnet, 40 + n)))
+        ifaces.append(wtf.node.Iface.create_iface(name="wlan1", driver="ath9k",
+                                                  ip="%s.%d" % (subnet, 40 + n),
+                                                  ops=ops))
         ifaces[-1].conf = wtf.node.mesh.MeshConf(ssid=meshid,
                                                  channel=1, htmode=htmode, iface=ifaces[-1], shared=False)
         ifaces.append(wtf.node.Iface(name="eth1"))
@@ -42,24 +46,26 @@ for n in range(10, 13):
 
 # extra guy to act as outside PC
 z_ssh = wtf.comm.SSH(ipaddr="10.10.10.13")
+ops = PlatformOps(z_ssh)
 z_ssh.name = "zotac-13"
 z_ssh.verbosity = 2
 ifaces = []
-ifaces.append(wtf.node.Iface(name="eth1"))
+ifaces.append(wtf.node.Iface.create_iface(name="eth1", ops=ops))
 ifaces[-1].enable = False
-z = wtf.node.mesh.MeshSTA(z_ssh, ifaces=ifaces)
+z = wtf.node.mesh.MeshSTA(z_ssh, ifaces=ifaces, ops=ops)
 zotacs.append(z)
 
 # configure your sniffer node here
 # ifaces=[]
 #ssh = wtf.comm.SSH(ipaddr="supersniffer.local")
+#ops = PlatformOps(ssh)
 #ssh.name = "sniffer"
 #ssh.verbosity = 2
-#ifaces.append(wtf.node.Iface(name="intel", driver="iwl4965"))
+#ifaces.append(wtf.node.Iface.create_iface(name="intel", driver="iwl4965", ops=ops))
 #ifaces[-1].conf = wtf.node.sniffer.SnifferConf(channel=1, htmode=htmode, iface=ifaces[-1])
-#ifaces.append(wtf.node.Iface(name="ath9k", driver="ath9k"))
+#ifaces.append(wtf.node.Iface(name="ath9k", driver="ath9k", ops=ops))
 ##ifaces[-1].conf = wtf.node.sniffer.SnifferConf(channel=1, htmode=htmode, iface=ifaces[-1])
-#sniffer = wtf.node.sniffer.SnifferSTA(ssh, ifaces=ifaces)
+#sniffer = wtf.node.sniffer.SnifferSTA(ssh, ifaces=ifaces, ops=ops)
 # zotacs.append(sniffer)
 
 # XXX: decouple testbed description from the specific test suite
